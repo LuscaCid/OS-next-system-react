7103159436e5c3209b7d3ae62b056c021dee3bca
Lucas Felipe Lima Cid <lucasfelipaa@gmail.com>
2024-02-26 10:32:43 -0300

    changes in styles

--- src/components/Home/HistoryComponents/JobsHistory.tsx
+++ src/components/Home/HistoryComponents/JobsHistory.tsx
@@ -7,13 +7,13 @@ export default function JobsHistory () {
     <div className="flex-col rounded-md w-full flex gap-1  p-1 items-center border border-zinc-300 dark:border-zinc-800/80">
         
           <input 
-          className="py-1 px-2  bg-zinc-300 rounded- w-full border border-zinc-400 text-md text-zinc-950 dark:bg-zinc-800 dark:text-zinc-300 dark:border-zinc-700/80"
+          className="py-1 px-2 rounded-sm bg-zinc-300 rounded- w-full border border-zinc-400 text-md text-zinc-950 dark:bg-zinc-800 dark:text-zinc-300 dark:border-zinc-700/80"
           type="text"
           placeholder="Pesquisar" 
           onChange={(e) => setQuery(e.target.value)}
         />
         
-        <div className=" border-t border-b w-full h-screen overflow-y-auto border-zinc-300 dark:border-zinc-800  rounded-md m-2 flex flex-col gap-2 max-h-[640px]">           
+        <div className=" border-t border-b w-full h-screen overflow-y-auto border-zinc-300 dark:border-zinc-800  rounded-md m-2 flex flex-col gap-2 pr-1 min-h-[550px] max-h-[520px]">           
          <HistoryFetchAndRendering query={query}/>
          
       </div> 
